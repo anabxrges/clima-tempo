a62071b930a785d38614c08d04ffa81be2f52f77
Ana Julia Borges <155864724+anabxrges@users.noreply.github.com>
2024-02-27 22:55:53 -0300

    dados js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const key= "6ccf415a079e46b9408ae39b87042845";
+const key = "6ccf415a079e46b9408ae39b87042845";
+
+async function buscarCidade (cidade) {
+  const dados = await fetch (`"https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}"`).then (resposta => resposta.json ());
+  
+  console.log (dados);
+}
+
+function cliqueiNoBotao () {
+  const cidade = document.querySelector (".input-cidade").value;
+  
+  buscarCidade(cidade);
+}
