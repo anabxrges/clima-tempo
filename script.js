b8ef953d90cc46d7710084be6e60356be10ce170
Ana Julia Borges <155864724+anabxrges@users.noreply.github.com>
2024-03-02 00:45:28 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-const key = "6ccf415a079e46b9408ae39b87042845"
+let key = "6ccf415a079e46b9408ae39b87042845";
 
 function colocarDadosNaTela(dados) {
-    console.log(dados)
-    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
-    document.querySelector(".temp").innerHTML = Math.floor (dados.main.temp) + "°C"
-    document.querySelector(".text-previsao").innerHTML =
-        dados.weather[0].description;
-        document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
-        document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
+    console.log(dados);
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name;
+    document.querySelector(".temp").innerHTML =
+        Math.floor(dados.main.temp) + "°C";
+    document.querySelector(".texto-previsao").innerHTML =
+        dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%";
+    document.querySelector(
+        ".img-previsao"
+    ).src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
 }
 
 async function buscarCidade(cidade) {
-    const dados = await fetch(
+    let dados = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt-BR&units=metric`
     ).then(resposta => resposta.json());
 
@@ -19,7 +22,7 @@ async function buscarCidade(cidade) {
 }
 
 function cliqueiNoBotao() {
-    const cidade = document.querySelector(".input-cidade").value;
+    let cidade = document.querySelector(".input-cidade").value;
 
     buscarCidade(cidade);
 }
